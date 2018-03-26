6598779685fd73a2c10e1a4cf5999c40c56a3717
Sean Oughton <seanoughton@gmail.com>
2018-03-26 21:30:11 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,6 +10,10 @@ class App < Sinatra::Base
     erb :create_puppy
   end
 
+  get '/create_puppy' do
+    erb :create_puppy
+  end
+
   post '/' do
     #text_from_user = params[:user_text]
     #@analyzed_text = TextAnalyzer.new(text_from_user)
