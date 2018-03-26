b79a5bffd09d4f7e77338247b6e139eb69085e8e
Sean Oughton <seanoughton@gmail.com>
2018-03-26 21:22:49 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,4 +6,10 @@ class App < Sinatra::Base
     erb :index
   end
 
+  post '/' do
+    #text_from_user = params[:user_text]
+    #@analyzed_text = TextAnalyzer.new(text_from_user)
+    #erb :results
+  end
+
 end
